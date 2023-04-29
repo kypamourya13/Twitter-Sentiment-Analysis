e9ad96a39ce037f3f5bc1dbad70497b4b21959f5
Mourya Sai Charan Reddy Kypa <36075316+Mouryasai27@users.noreply.github.com>
2023-04-29 10:36:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ class SentimentAnalysis:
 
     def DownloadData(self):
         # authenticating
-        consumerKey = 'your key here'
-        consumerSecret = 'your key here'
-        accessToken = 'your key here'
-        accessTokenSecret = 'your key here'
+        consumerKey = 'E6HyZknTHB2aCSSvCc2VbAaGW'
+        consumerSecret = '48Mzv1UY8hfdOkuv2cTlJiZJBsUYZCTSa3vLmwsEq9cBy3W93K'
+        accessToken = '740515434949054464-Skot37kT0yHK8lvflEeYdJ5dS824aul'
+        accessTokenSecret = 'Cqk2l3dMeR2b98kEcvZW2Okqxkx2TZampi5t3Xh3ViOI4'
         auth = tweepy.OAuthHandler(consumerKey, consumerSecret)
         auth.set_access_token(accessToken, accessTokenSecret)
         api = tweepy.API(auth)
@@ -36,16 +36,13 @@ class SentimentAnalysis:
         # creating some variables to store info
         polarity = 0
         positive = 0
-        wpositive = 0
-        spositive = 0
         negative = 0
-        wnegative = 0
-        snegative = 0
         neutral = 0
 
 
         # iterating through tweets fetched
         for tweet in self.tweets:
+            #print(tweet.text)
             #Append to temp so that we can store in csv later. I use encode UTF-8
             self.tweetText.append(self.cleanTweet(tweet.text).encode('utf-8'))
             # print (tweet.text.translate(non_bmp_map))    #print tweet's text
@@ -55,18 +52,10 @@ class SentimentAnalysis:
 
             if (analysis.sentiment.polarity == 0):  # adding reaction of how people are reacting to find average later
                 neutral += 1
-            elif (analysis.sentiment.polarity > 0 and analysis.sentiment.polarity <= 0.3):
-                wpositive += 1
-            elif (analysis.sentiment.polarity > 0.3 and analysis.sentiment.polarity <= 0.6):
+            elif (analysis.sentiment.polarity > 0):
                 positive += 1
-            elif (analysis.sentiment.polarity > 0.6 and analysis.sentiment.polarity <= 1):
-                spositive += 1
-            elif (analysis.sentiment.polarity > -0.3 and analysis.sentiment.polarity <= 0):
-                wnegative += 1
-            elif (analysis.sentiment.polarity > -0.6 and analysis.sentiment.polarity <= -0.3):
+            elif (analysis.sentiment.polarity < 0):
                 negative += 1
-            elif (analysis.sentiment.polarity > -1 and analysis.sentiment.polarity <= -0.6):
-                snegative += 1
 
 
         # Write to csv and close csv file
@@ -75,11 +64,7 @@ class SentimentAnalysis:
 
         # finding average of how people are reacting
         positive = self.percentage(positive, NoOfTerms)
-        wpositive = self.percentage(wpositive, NoOfTerms)
-        spositive = self.percentage(spositive, NoOfTerms)
         negative = self.percentage(negative, NoOfTerms)
-        wnegative = self.percentage(wnegative, NoOfTerms)
-        snegative = self.percentage(snegative, NoOfTerms)
         neutral = self.percentage(neutral, NoOfTerms)
 
         # finding average reaction
@@ -87,35 +72,23 @@ class SentimentAnalysis:
 
         # printing out data
         print("How people are reacting on " + searchTerm + " by analyzing " + str(NoOfTerms) + " tweets.")
-        print()
-        print("General Report: ")
+        #print()
+        #print("General Report: ")
 
-        if (polarity == 0):
-            print("Neutral")
-        elif (polarity > 0 and polarity <= 0.3):
-            print("Weakly Positive")
-        elif (polarity > 0.3 and polarity <= 0.6):
-            print("Positive")
-        elif (polarity > 0.6 and polarity <= 1):
-            print("Strongly Positive")
-        elif (polarity > -0.3 and polarity <= 0):
-            print("Weakly Negative")
-        elif (polarity > -0.6 and polarity <= -0.3):
-            print("Negative")
-        elif (polarity > -1 and polarity <= -0.6):
-            print("Strongly Negative")
+       # if (polarity == 0):
+        #    print("Neutral")
+       # elif (polarity < 0):
+        #    print("Negative")
+       # elif (polarity > 0):
+       #     print("Positive")
 
         print()
         print("Detailed Report: ")
         print(str(positive) + "% people thought it was positive")
-        print(str(wpositive) + "% people thought it was weakly positive")
-        print(str(spositive) + "% people thought it was strongly positive")
         print(str(negative) + "% people thought it was negative")
-        print(str(wnegative) + "% people thought it was weakly negative")
-        print(str(snegative) + "% people thought it was strongly negative")
         print(str(neutral) + "% people thought it was neutral")
 
-        self.plotPieChart(positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, NoOfTerms)
+        self.plotPieChart(positive, negative, neutral, searchTerm, NoOfTerms)
 
 
     def cleanTweet(self, tweet):
@@ -127,11 +100,10 @@ class SentimentAnalysis:
         temp = 100 * float(part) / float(whole)
         return format(temp, '.2f')
 
-    def plotPieChart(self, positive, wpositive, spositive, negative, wnegative, snegative, neutral, searchTerm, noOfSearchTerms):
-        labels = ['Positive [' + str(positive) + '%]', 'Weakly Positive [' + str(wpositive) + '%]','Strongly Positive [' + str(spositive) + '%]', 'Neutral [' + str(neutral) + '%]',
-                  'Negative [' + str(negative) + '%]', 'Weakly Negative [' + str(wnegative) + '%]', 'Strongly Negative [' + str(snegative) + '%]']
-        sizes = [positive, wpositive, spositive, neutral, negative, wnegative, snegative]
-        colors = ['yellowgreen','lightgreen','darkgreen', 'gold', 'red','lightsalmon','darkred']
+    def plotPieChart(self, positive, negative, neutral, searchTerm, noOfSearchTerms):
+        labels = ['Positive [' + str(positive) + '%]','Neutral [' + str(neutral) + '%]','Negative [' + str(negative) + '%]']
+        sizes = [positive,neutral, negative]
+        colors = ['yellowgreen', 'gold', 'red']
         patches, texts = plt.pie(sizes, colors=colors, startangle=90)
         plt.legend(patches, labels, loc="best")
         plt.title('How people are reacting on ' + searchTerm + ' by analyzing ' + str(noOfSearchTerms) + ' Tweets.')
